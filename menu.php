6bd355b3500f2a0bd2cb5b8161a7875263203d3e
Filip Procházka <filip@prochazka.su>
2014-10-29 16:50:35 +0100

    Fixed menu.php

--- menu.php
+++ menu.php
@@ -1,27 +1,19 @@
-<html>
-<head>
-    <meta http-equiv="content-type" content="text/html;" charset="utf-8">
-    <link rel="stylesheet"  type="text/css" href="css/menu.css">
-</head>
-<body>
-    <ul class="menu uroven1">
-        <li class="polozka1">
-<!--            <a href="index.php">Home</a>-->
-            <a href="index.php">Úvod</a>
-        </li>
-        <li class="polozka1">
-            <a href="festival.php">Festival</a>
-        </li>
-        <li class="polozka1">
-            <a href="predesle_roky.php">Historie</a>
-        </li>
-        <li class="polozka1">
-            <a href="fotogalerie.php">Galerie</a>
-        </li>
-        <li class="polozka1">
-            <a href="kontakt.php">Kontakt</a>
-        </li>
-    </ul>
-</body>
-</html>
+<ul class="menu uroven1">
+    <li class="polozka1">
+<!--    <a href="index.php">Home</a>-->
+        <a href="index.php">Úvod</a>
+    </li>
+    <li class="polozka1">
+        <a href="festival.php">Festival</a>
+    </li>
+    <li class="polozka1">
+        <a href="predesle_roky.php">Historie</a>
+    </li>
+    <li class="polozka1">
+        <a href="fotogalerie.php">Galerie</a>
+    </li>
+    <li class="polozka1">
+        <a href="kontakt.php">Kontakt</a>
+    </li>
+</ul>
 
